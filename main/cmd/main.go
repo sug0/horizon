ef2c53a1e5e9da09b8482b906b0096b23ca2955a
Tiago Carvalho <sugoiuguu@tfwno.gf>
2020-05-25 23:49:59 +0100

    support custom scripts

--- main/cmd/main.go
+++ main/cmd/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
     "os"
+    "flag"
     "time"
     "context"
     "io/ioutil"
@@ -11,41 +12,47 @@ import (
     "github.com/sug0/horizon/script"
 )
 
-const glitchProgram = `
-max := func(x, y) {
-    if x > y {
-        return x
-    }
-    return y
-}
-
-// check if we saved last values
-if !ctx.r {
-    ctx.r = pixel[0]
-    ctx.g = pixel[1]
-    ctx.b = pixel[2]
-}
-
-pixel[0] = x & i64(1.5*f64(max(pixel[0], pixel[1]) - ctx.r))
-pixel[1] = y & i64(1.5*f64(max(pixel[1], pixel[2]) - ctx.g))
-pixel[2] = x & i64(1.5*f64(max(pixel[2], pixel[0]) - ctx.b))
-`
-
 func main() {
-    s, err := script.Compile([]byte(glitchProgram))
+    var imagePath string
+    var outPath string
+    var glitchProgramPath string
+
+    flag.StringVar(&imagePath, "in", "-", "The input image path. If empty, reads from stdin.")
+    flag.StringVar(&outPath, "out", "-", "The output image path. If empty, writes to stdout.")
+    flag.StringVar(&glitchProgramPath, "script", "", "The glitching script path.")
+    flag.Parse()
+
+    if glitchProgramPath == "" {
+        panic("github.com/sug0/horizon/main/cmd: you need to specify a glitch script")
+    }
+    glitchProgram, err := ioutil.ReadFile(glitchProgramPath)
     if err != nil {
         panic(err)
     }
-    bufdata, err := ioutil.ReadAll(os.Stdin)
+    s, err := script.Compile(glitchProgram)
     if err != nil {
         panic(err)
+    }
+
+    var bufdata []byte
+    if imagePath == "-" {
+        bufdata, err = ioutil.ReadAll(os.Stdin)
+        if err != nil {
+            panic(err)
+        }
+    } else {
+        bufdata, err = ioutil.ReadFile(imagePath)
+        if err != nil {
+            panic(err)
+        }
     }
     mat, err := gocv.IMDecode(bufdata, gocv.IMReadColor)
     if err != nil {
         panic(err)
     }
     defer mat.Close()
-    ctx, cancel := context.WithTimeout(context.Background(), 3 * time.Minute)
+
+    ctx, cancel := context.WithTimeout(context.Background(), 5 * time.Minute)
     defer cancel()
     newmat, err := horizon.Glitch(ctx, s, mat)
     if err != nil {
@@ -56,5 +63,15 @@ func main() {
     if err != nil {
         panic(err)
     }
-    os.Stdout.Write(bufdata)
+    if outPath == "-" {
+        _, err = os.Stdout.Write(bufdata)
+        if err != nil {
+            panic(err)
+        }
+        return
+    }
+    err = ioutil.WriteFile(outPath, bufdata, 0644)
+    if err != nil {
+        panic(err)
+    }
 }
