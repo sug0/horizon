3f1c8b647e041fce8cee6824b73e162f889e8ca2
Tiago Carvalho <sugoiuguu@tfwno.gf>
2020-05-25 07:50:24 +0100

    trying to use this package from discord but it fails so fuck you going to sleep

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/sug0/horizon
 
 go 1.14
 
-require github.com/d5/tengo/v2 v2.4.2
+require (
+	github.com/d5/tengo/v2 v2.4.2
+	github.com/sug0/lilliput v0.0.0-20200525063604-8165bcb355ad // indirect
+)
--- main/cmd/main.go
+++ main/cmd/main.go
@@ -1,34 +1,44 @@
 package main
 
 import (
-    "fmt"
+    "os"
+    "unsafe"
+    "strconv"
+    "io/ioutil"
 
     "github.com/d5/tengo/v2"
-    "github.com/d5/tengo/v2/parser"
+    "github.com/sug0/lilliput"
+    "github.com/sug0/horizon/script"
 )
-
-type script struct {
-    bytecode *tengo.Bytecode
-}
-
-var (
-    symbols *tengo.SymbolTable
-    modules *tengo.ModuleMap
-)
-
-func init() {
-    symbols = tengo.NewSymbolTable()
-    modules = tengo.NewModuleMap()
-
-    // globals[0] --> pixel
-    symbols.Define("pixel")
-}
 
 func main() {
-    s, err := compileScript([]byte(`pixel[0] += 5`))
+    s, err := script.Compile([]byte(`pixel[0] += 5`))
     if err != nil {
         panic(err)
     }
+    bufdata, err := ioutil.ReadAll(os.Stdin)
+    if err != nil {
+        panic(err)
+    }
+    decoder, err := lilliput.NewDecoder(bufdata)
+    if err != nil {
+        panic(err)
+    }
+    defer decoder.Close()
+    header, err := decoder.Header()
+    if err != nil {
+        panic(err)
+    }
+    pixtyp := header.PixelType()
+    if pixtyp.Depth() != 8 {
+        panic("unsupported depth: " + strconv.Itoa(pixtyp.Depth()))
+    }
+    if pixtyp.Channels() != 3 {
+        panic("unsupported channels: " + strconv.Itoa(pixtyp.Channels()))
+    }
+    framebuf := lilliput.NewFramebuffer(header.Width(), header.Height())
+    defer framebuf.Clear()
+    bitmap := ((*struct { Buf []byte })(unsafe.Pointer(framebuf))).Buf
     pixel := tengo.Array{
         Value: []tengo.Object{
             &tengo.Int{},
@@ -36,12 +46,7 @@ func main() {
             &tengo.Int{},
         },
     }
-    vm := s.bootstrapVM(&pixel)
-    bitmap := []int64{
-        5, 4, 1,
-        2, 1, 0,
-        0, 4, 2,
-    }
+    vm := s.BootstrapVM(&pixel)
     for i := 0; i < len(bitmap); i += 3 {
         // set pixel value
         setPixel(&pixel, 0, bitmap[i+0])
@@ -65,27 +70,4 @@ func main() {
 func setPixel(pixel *tengo.Array, i int, x int64) {
     v := pixel.Value[i].(*tengo.Int)
     v.Value = x
-}
-
-func (s script) bootstrapVM(pixel *tengo.Array) *tengo.VM {
-    globals := []tengo.Object{pixel}
-    return tengo.NewVM(s.bytecode, globals, -1)
-}
-
-func compileScript(source []byte) (s script, err error) {
-    var file *parser.File
-    fileSet := parser.NewFileSet()
-    srcFile := fileSet.AddFile("(main)", -1, len(source))
-    p := parser.NewParser(srcFile, source, nil)
-    file, err = p.ParseFile()
-    if err != nil {
-        return
-    }
-    c := tengo.NewCompiler(srcFile, symbols, nil, modules, nil)
-    err = c.Compile(file)
-    if err != nil {
-        return
-    }
-    s.bytecode = c.Bytecode()
-    return
 }
