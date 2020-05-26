4dbb7d3c80b16a25b0242e47cbdc46109c0d377b
Tiago Carvalho <sugoiuguu@tfwno.gf>
2020-05-26 02:25:18 +0100

    remove init()

--- script/compile.go
+++ script/compile.go
@@ -9,16 +9,46 @@ import (
 
 type Script struct {
     *tengo.Bytecode
-}
 
-var (
     symbols *tengo.SymbolTable
     modules *tengo.ModuleMap
-)
+}
 
-func init() {
+func (s *Script) BootstrapVM(globals ...tengo.Object) *tengo.VM {
+    actualGlobals := make([]tengo.Object, 32)
+    for i := 0; i < len(globals); i++ {
+        actualGlobals[i] = globals[i]
+    }
+    return tengo.NewVM(s.Bytecode, actualGlobals, -1)
+}
+
+func Compile(source []byte) (*Script, error) {
+    fileSet := parser.NewFileSet()
+    srcFile := fileSet.AddFile("(main)", -1, len(source))
+    p := parser.NewParser(srcFile, source, nil)
+    file, err := p.ParseFile()
+    if err != nil {
+        err = fmt.Errorf("script: failed to parse script: %w", err)
+        return nil, err
+    }
+    symbols := buildSymTab()
+    modules := tengo.NewModuleMap()
+    c := tengo.NewCompiler(srcFile, symbols, nil, modules, nil)
+    err = c.Compile(file)
+    if err != nil {
+        err = fmt.Errorf("script: failed to compile script: %w", err)
+        return nil, err
+    }
+    s := &Script{
+        Bytecode: c.Bytecode(),
+        modules: modules,
+        symbols: symbols,
+    }
+    return s, nil
+}
+
+func buildSymTab() (symbols *tengo.SymbolTable) {
     symbols = tengo.NewSymbolTable()
-    modules = tengo.NewModuleMap()
 
     // globals[0] --> output
     symbols.Define("output")
@@ -46,30 +76,6 @@ func init() {
 
     // globals[8] --> ctx
     symbols.Define("ctx")
-}
 
-func (s *Script) BootstrapVM(globals ...tengo.Object) *tengo.VM {
-    actualGlobals := make([]tengo.Object, 32)
-    for i := 0; i < len(globals); i++ {
-        actualGlobals[i] = globals[i]
-    }
-    return tengo.NewVM(s.Bytecode, actualGlobals, -1)
-}
-
-func Compile(source []byte) (*Script, error) {
-    fileSet := parser.NewFileSet()
-    srcFile := fileSet.AddFile("(main)", -1, len(source))
-    p := parser.NewParser(srcFile, source, nil)
-    file, err := p.ParseFile()
-    if err != nil {
-        err = fmt.Errorf("script: failed to parse script: %w", err)
-        return nil, err
-    }
-    c := tengo.NewCompiler(srcFile, symbols, nil, modules, nil)
-    err = c.Compile(file)
-    if err != nil {
-        err = fmt.Errorf("script: failed to compile script: %w", err)
-        return nil, err
-    }
-    return &Script{c.Bytecode()}, nil
+    return
 }
